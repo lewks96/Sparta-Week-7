6539897df51b0fce81d42ddca3db5870724c83f4
Lewis Kellett <lewisk1996@gmail.com>
2022-10-27 15:55:13 +0100

    Work from today

---
+++ Projects/MvcMovie/MvcMovie/Migrations/20221027142459_Poster.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    public partial class Poster : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "Movie",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Movie");
+        }
+    }
+}
